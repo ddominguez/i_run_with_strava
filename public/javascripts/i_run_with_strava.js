dda611d3c65bd17a83e382293afc805841d1198e
David Dominguez <ddominguez@gmail.com>
2016-01-10 15:58:13 -0500

    Show strava activities

---
+++ public/javascripts/i_run_with_strava.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '/strava_activities',
+        type: 'GET'
+    })
+    .success(function(data, textStatus, jqXHR) {
+        show_activities(data);
+    });
+});
+
+function format_data(date_string) {
+    d = new Date(date_string);
+    return d.getUTCHours()+":"+d.getUTCMinutes();
+}
+
+function show_activities(activities) {
+    var activities_length = activities.length;
+    for (var i=0; i < activities_length; i++) {
+        build_activity(activities[i]);
+    }
+}
+
+function build_activity(activity) {
+    var activity_row = '<div class="row">'
+    +'<div class="table-cell">'+activity.name+'</div>'
+    +'<div class="table-cell">'+format_data(activity.start_date_local)+'</div>'
+    +'</div>';
+    $('#strava-activities').append(activity_row);
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var STRAVA_TOKEN = process.env.STRAVA_TOKEN || null;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'I Run With Strava'
+    });
 });
+
+
+router.get('/strava_activities', function(req, res, next) {
+    // check if token exists
+    if (!STRAVA_TOKEN) {
+        res.send('token not found.')
+    }
+
+    var options = {
+        url: 'https://www.strava.com/api/v3/athlete/activities?per_page=10',
+        headers: {
+            'Authorization': 'Bearer ' + STRAVA_TOKEN
+        }
+    };
+    var callback = function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.send(data);
+        } else {
+            res.send('There was a problem with the request.')
+        }
+    };
+    var activities = request(options, callback);
+})
 
 module.exports = router;
