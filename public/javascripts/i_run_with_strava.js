cc05092cdf57242e3f5a29a6b6178b30da8e4e9f
David Dominguez <ddominguez@gmail.com>
2016-01-18 02:37:23 -0500

    Added google map for activity

--- public/javascripts/i_run_with_strava.js
+++ public/javascripts/i_run_with_strava.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(document).ready(function() {
+    // get strava activities
     $.ajax({
         url: '/strava_activities',
         type: 'GET'
@@ -11,6 +12,9 @@ $(document).ready(function() {
     .fail(function(data, textStatus, jqXHR) {
         showError(data.responseText);
     });
+
+    // show activity map
+    $('#strava-activities').on('click', 'div.activity', showMap);
 });
 
 function formatDate(date_string) {
@@ -26,7 +30,7 @@ function showActivities(activities) {
 }
 
 function buildActivity(activity) {
-    var activity_row = '<div class="row">'
+    var activity_row = '<div class="row activity" data-lat="'+activity.start_latitude+'" data-lng="'+activity.start_longitude+'" data-polyline="'+activity.map.summary_polyline+'">'
     +'<div class="table-cell"><a href="//www.strava.com/activities/'+activity.id+'">'+activity.name+'</a></div>'
     +'<div class="table-cell">'+formatDate(activity.start_date_local)+'</div>'
     +'<div class="table-cell">'+activity.location_city+', '+activity.location_state+'</div>'
@@ -60,4 +64,55 @@ function getMonth(i) {
 function showError(message) {
     $('#strava-activities').html('An error has occurred.');
     console.error(message);
+}
+
+function showMap() {
+    $('#map').hide();
+    createMap($(this).data('lat'), $(this).data('lng'), $(this).data('polyline'));
+}
+
+function createMap(latitude, longitude, polyline) {
+    var mapOptions = {
+      center: new google.maps.LatLng(latitude, longitude),
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var decodedPath = google.maps.geometry.encoding.decodePath(polyline);
+    var decodedLevels = decodeLevels("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+    var poly = new google.maps.Polyline({
+        path: decodedPath,
+        levels: decodedLevels,
+        strokeColor: '#ff0000',
+        strokeOpacity: 1,
+        strokeWeight: 2,
+    });
+    poly.setMap(map);
+    zoomToObject(poly, map);
+    $('#map').show();
+}
+
+function decodeLevels(encodedLevelsString) {
+    var decodedLevels = [];
+
+    for (var i = 0; i < encodedLevelsString.length; ++i) {
+        var level = encodedLevelsString.charCodeAt(i) - 63;
+        decodedLevels.push(level);
+    }
+    return decodedLevels;
+}
+
+function zoomToObject(obj, map){
+    var bounds = new google.maps.LatLngBounds();
+    var points = obj.getPath().getArray();
+    for (var n = 0; n < points.length ; n++){
+        bounds.extend(points[n]);
+    }
+    map.fitBounds(bounds);
+    setTimeout(function() {
+                google.maps.event.trigger(map,'resize');
+                map.fitBounds(bounds);
+                }, 200);
 }
